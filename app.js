760d62d85a624cd854ff5beaf7dadc545bb3f643
pratappokala <110607713+pratappokala@users.noreply.github.com>
2022-12-02 12:54:00 +0530

    fcm-iframe

---
+++ app.js
@@ -0,0 +1,53 @@
+const firebaseConfig = {
+          
+    apiKey: "AIzaSyDLOosAg3k-xQd0bXiMqulMyMd6GsWbMaE",
+    authDomain: "ocb-tizen-d78a1.firebaseapp.com",
+    projectId: "ocb-tizen-d78a1",
+    storageBucket: "ocb-tizen-d78a1.appspot.com",
+    messagingSenderId: "277255948303",
+    appId: "1:277255948303:web:afa51467c6ff4dacd3fb7a",
+    measurementId: "G-ZXKPNLFRRE"
+  
+  };
+      
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+
+const messaging = firebase.messaging();
+
+// isSupported();
+
+
+function SubscribeUsers() {
+Notification.requestPermission().then(Permission => {
+console.log(Permission);
+if(Permission == "granted"){
+ 
+  if(Permission == "granted"){
+    messaging.getToken(messaging, {
+    vapidKey:
+      "BAvMwMWlkmz0HTGdk_jOXp3NsPG6yRUt2a6XyGp-Ad5hxpaf-eosqVRKCSY6AYXbraLN5tprnHlLfnBVwj1USLY",
+  })
+    .then((currentToken) => {
+      if (currentToken) {
+        console.log("Firebase Token", currentToken);
+      } else {
+       
+        console.log(
+          "No registration token available. Request permission to generate one."
+        );
+       
+      }
+    })
+    .catch((err) => {
+      console.log("An error occurred while retrieving token. ", err);
+     
+    });
+  
+  
+}
+}
+});
+}
+
+SubscribeUsers();
---
+++ firebase-messaging-sw.js
@@ -0,0 +1,29 @@
+
+importScripts('https://www.gstatic.com/firebasejs/9.9.0/firebase-app-compat.js');
+importScripts('https://www.gstatic.com/firebasejs/9.9.0/firebase-messaging-compat.js');
+
+
+ if ('serviceWorker' in navigator) {
+     navigator.serviceWorker.register('../firebase-messaging-sw.js')
+       .then(function(registration) {
+         console.log('Registration successful, scope is:', registration.scope);
+       }).catch(function(err) {
+        console.log('Service worker registration failed, error:', err);
+       });
+    }
+    self.addEventListener('push', function (event) {
+      console.log('Service Worker recived a push message', event.data.text());
+      var notification = event.data.json().notification;
+      var title = notification.title;
+      event.waitUntil(
+          self.registration.showNotification(title, {
+              body: notification.body,
+              icon: notification.icon,
+              data: { url: notification.click_action }
+          }));
+    });
+ firebase.initializeApp({
+    // messagingSenderId: "410451915353",
+    messagingSenderId: "277255948303",
+   })
+
